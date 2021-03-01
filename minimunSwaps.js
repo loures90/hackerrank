f404816ad134c61d2bd2128d71f2b2d6534d993d
Fernando <Fernando>
2021-03-01 16:04:18 -0300

    minimum swaps

---
+++ minimunSwaps.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the minimumSwaps function below.
+function minimumSwaps(arr) {
+    let count = 0
+    for(let index = 0; index < arr.length - 1; index++){
+      if(arr[index] !== index+1){
+        let num = arr[index]
+        arr[arr.indexOf(index+1,index)] =num
+        arr[index] = index+1
+        count += 1
+        }
+    }
+
+
+    return count
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const res = minimumSwaps(arr);
+
+    ws.write(res + '\n');
+
+    ws.end();
+}
